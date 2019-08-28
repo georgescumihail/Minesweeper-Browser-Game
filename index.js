ab0f43068ff2e880c093ac340905084160332b2c
Mihail Georgescu <geo.mihail96@gmail.com>
2019-08-28 08:16:49 +0300

    Field sections are now clickable & other gameplay mechanisms implemented

--- index.js
+++ index.js
@@ -1,65 +1,69 @@
 var gamefield = []
+var visited = []
 var mudHoles = []
 
 var tableSize = 10;
 
-generateField();
-generateMud();
-calculateValues();
-
-
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
 
-canvas.width = 400;
-canvas.height = 400;
+var canvasSize = 400;
 
+canvas.width = canvasSize;
+canvas.height = canvasSize;
 
-drawGrid();
+var sectionSize = canvasSize / tableSize;
 
-for (var i = 0; i < tableSize; i++) {
-    for (var j = 0; j < tableSize; j++) {
+canvas.addEventListener('mousedown', function (e) {
 
-        if (gamefield[i][j] == 99) {
-
-            context.fillStyle = '#775511';
-            context.fillRect(j * (canvas.height / tableSize), i * (canvas.height / tableSize), canvas.height / tableSize, canvas.height / tableSize);
-        }
-
-        else if (gamefield[i][j] > 0) {
-            context.font = '25px Comic Sans MS';
-            context.fillStyle = '#AAFFAA';
-            context.fillRect(j * (canvas.height / tableSize), i * (canvas.height / tableSize), canvas.height / tableSize, canvas.height / tableSize);
-            context.fillStyle = '#EE4444';
-            context.fillText(gamefield[i][j], j * (canvas.height / tableSize) + canvas.height / tableSize / 3, i * (canvas.height / tableSize) + canvas.height / tableSize / 1.4);
-        }
-        else {
-            context.font = '25px Comic Sans MS';
-            context.fillStyle = '#AAFFAA';
-            context.fillRect(j * (canvas.height / tableSize), i * (canvas.height / tableSize), canvas.height / tableSize, canvas.height / tableSize);
-            context.fillStyle = '#111111';
-            context.fillText(gamefield[i][j], j * (canvas.height / tableSize) + canvas.height / tableSize / 3, i * (canvas.height / tableSize) + canvas.height / tableSize / 1.4);
-        }
+    if (e.button == 1) {
+        clickedSection(e);
     }
 
+    if (e.button == 2) {
+        rightClickedSection(e);
+    }
+});
+
+
+var restartButton = document.getElementById('restart');
+restartButton.addEventListener('click', startGame);
+
+var lost = false;
+
+startGame();
+
+function startGame() {
+
+    context.clearRect(0, 0, canvasSize, canvasSize);
+    context.globalAlpha = 1;
+    generateField();
+    generateMud();
+    calculateValues();
+
+    context.fillStyle = '#66DD66';
+    context.fillRect(0, 0, canvasSize, canvasSize);
+
+    drawGrid();
 }
 
-drawGrid();
 
 function drawGrid() {
     for (var i = 0; i <= tableSize; i++) {
 
-        context.strokeStyle = '#111111';
+        context.strokeStyle = '#000000';
+        context.globalAlpha = 0.8;
         context.beginPath();
         context.moveTo(0, i * (canvas.height / tableSize));
         context.lineTo(600, i * (canvas.height / tableSize));
+        context.stroke();
         context.stroke();
 
         context.beginPath();
         context.moveTo(i * (canvas.height / tableSize), 0);
         context.lineTo(i * (canvas.height / tableSize), 600);
         context.stroke();
-
+        context.stroke();
     }
 
     context.strokeRect(0, 0, canvas.width, canvas.height);
@@ -75,11 +79,19 @@ function generateField() {
         }
     }
 
-}
+    for (var i = 0; i < tableSize; i++) {
+        visited[i] = [];
 
+        for (var j = 0; j < tableSize; j++) {
+            visited[i][j] = false;
+        }
+    }
+
+}
 
 function generateMud() {
 
+    mudHoles = []
     var tries = tableSize;
 
     for (var i = 0; i < tries; i++) {
@@ -90,7 +102,6 @@ function generateMud() {
         if (mudHoles.some(m => m.x === randX) && mudHoles.some(m => m.y === randY)) {
             tries++;
         }
-
         else {
             gamefield[randX][randY] = 99;
             mudHoles.push({ x: randX, y: randY });
@@ -149,6 +160,89 @@ function calculateValues() {
 
         }
     }
+}
+
+function clickedSection(e) {
+
+    var coordX = e.clientX - canvas.offsetLeft;
+    var coordY = e.clientY - canvas.offsetTop + window.pageYOffset;
+
+    var iSelected = Math.floor(coordY / sectionSize);
+    var jSelected = Math.floor(coordX / sectionSize);
+
+    if (visited[iSelected][jSelected] == false && lost == false) {
+
+        drawSection(iSelected, jSelected);
+    }
+
+}
+
+/*
+function rightClickedSection(e) {
+
+    var coordX = e.clientX - canvas.offsetLeft;
+    var coordY = e.clientY - canvas.offsetTop + window.pageYOffset;
+
+    var iSelected = Math.floor(coordY / sectionSize);
+    var jSelected = Math.floor(coordX / sectionSize);
+
+    if (visited[iSelected][jSelected] == 99 && lost == false) {
+
+        context.fillStyle = '#775511';
+        context.fillRect(j * sectionSize, i * sectionSize, sectionSize, sectionSize);
+    }
+}
+
+*/
+
+function drawSection(i, j) {
+
+    if (gamefield[i][j] == 99) {
+
+        context.fillStyle = '#775511';
+        context.fillRect(j * sectionSize, i * sectionSize, sectionSize, sectionSize);
+
+        lost = true;
+        lostDisplay();
+    }
+
+    else if (gamefield[i][j] > 0) {
+        context.font = '25px Comic Sans MS';
+        context.fillStyle = '#AAFFAA';
+        context.fillRect(j * sectionSize, i * sectionSize, sectionSize, sectionSize);
+        context.fillStyle = '#EE4444';
+        context.fillText(gamefield[i][j], j * sectionSize + sectionSize / 3, i * sectionSize + sectionSize / 1.4);
+    }
+    else {
+        context.fillStyle = '#AAFFAA';
+        context.fillRect(j * sectionSize, i * sectionSize, sectionSize, sectionSize);
+    }
+    visited[i][j] = true;
+
+    if (gamefield[i][j] == 0) {     // recursive call to reveal all sections around an empty section if they are empty as well
+
+        if (i > 0 && visited[i - 1][j] == false) { drawSection(i - 1, j); }
+        if (j > 0 && visited[i][j - 1] == false) { drawSection(i, j - 1); }
+        if (i < tableSize - 1 && visited[i + 1][j] == false) { drawSection(i + 1, j); }
+        if (j < tableSize - 1 && visited[i][j + 1] == false) { drawSection(i, j + 1); }
+        if (i > 0 && j > 0 && visited[i - 1][j - 1] == false) { drawSection(i - 1, j - 1); }
+        if (i > 0 && j < tableSize - 1 && visited[i - 1][j + 1] == false) { drawSection(i - 1, j + 1); }
+        if (i < tableSize - 1 && j > 0 && visited[i + 1][j - 1] == false) { drawSection(i + 1, j - 1); }
+        if (i < tableSize - 1 && j < tableSize - 1 && visited[i + 1][j + 1] == false) { drawSection(i + 1, j + 1); }
+    }
+    else {
+
+        return;
+    }
+
+    console.log(i + " " + j);
+}
+
+
+function lostDisplay() {
+
+    var lostMessage = document.getElementById('lost-message');
+    lostMessage.innerText = "You fell in the mud puddle. Try again!";
 }
 
 console.log(gamefield);
